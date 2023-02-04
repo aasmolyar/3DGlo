12a6953a25181f3983052ca5328c8d3f59326181
Andrey Smolyar <smolyar2022@internet.ru>
2023-02-04 12:09:56 +0500

    modal with innerHeight

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -4,22 +4,11 @@ const modal = () => {
     const buttons = document.querySelectorAll('.popup-btn');
     const closeBtn = modal.querySelector('.popup-close');
 
-    let height = document.documentElement.clientHeight;
-    let width = document.documentElement.clientWidth;
-
-    window.addEventListener('resize', () => {
-    height = document.documentElement.clientHeight;
-    width = document.documentElement.clientWidth;
-
-    console.log('height',height);
-    console.log('width', width);
-    }); 
-
     buttons.forEach(btn => {
         btn.addEventListener('click', () => {
         modal.style.display = 'block';
 
-        if (height > 768 || width > 768) {
+        if (window.innerHeight > 768 || window.innerWidth > 768) {
             modalAnikmaiton();
             }
         });
